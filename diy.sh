27b6d1999cf3549fbd0a6fc56af2b8a26f7edc85
pergian <pergian@users.noreply.github.com>
2020-03-24 14:38:10 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,9 +2,11 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: pergian
+# Blog: https://pergianhkcentos.tk
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/destan19/openwrt.git package/
+./scripts/feeds update -a
+./scripts/feeds install -a
