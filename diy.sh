78b2bf6069f109873f4a2bfc7ed3c2fad9e8044a
pergian <pergian@users.noreply.github.com>
2020-05-26 12:59:27 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,11 +3,13 @@
 # Description: DIY script
 # Lisence: MIT
 # Author: pergian
-# Blog: https://pergianhkcentos.tk
+# Email: pergian@qq.com
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/zerotier/ZeroTierOne.git  package/ZeroTierOne
+git clone https://github.com/nondanee/UnblockNeteaseMusic.git package/UnblockNeteaseMusic
 ./scripts/feeds update -a
 ./scripts/feeds install -a
